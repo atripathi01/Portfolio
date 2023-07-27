2d84b4ce2e3391d8251ef763f9898046e430e618
ayusht001 <ayush.tripathi@qwikskills.com>
2023-07-27 21:37:49 +0530

    key issue

--- components/Skills/SkillsCard.js
+++ components/Skills/SkillsCard.js
@@ -10,37 +10,35 @@ const SkillsCard = ({ toolNPlatform, webdeveloper }) => {
   }, []);
   return (
     <div >
-    <div className="flex justify-start laptop:flex-row mob:flex-col  my-7 align-middle ">
-      <h1 className="text-3xl laptop:w-1/3 mob:w-full">Web-Development</h1>
-      <div className=" laptop:w-2/3 mob:w-full flex flex-wrap">
-        {webdeveloper?.map((list,index)=><div
-                           key={index}
-          className={`w-fit p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
-            mounted && theme === "dark"
-              ? "hover:bg-slate-800"
-              : "hover:bg-slate-50"
-          } hover:scale-105 link`}
-        >
-          <h1 className="text-xl">{list}</h1>
+      <div className="flex justify-start laptop:flex-row mob:flex-col  my-7 align-middle ">
+        <h1 className="text-3xl laptop:w-1/3 mob:w-full">Web-Development</h1>
+        <div className=" laptop:w-2/3 mob:w-full flex flex-wrap">
+          {webdeveloper?.map((list, index) => <div
+            key={index}
+            className={`w-fit p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${mounted && theme === "dark"
+                ? "hover:bg-slate-800"
+                : "hover:bg-slate-50"
+              } hover:scale-105 link`}
+          >
+            <h1 className="text-xl">{list}</h1>
+          </div>
+          )}
         </div>
-        )}
       </div>
-    </div>
-    <div className="flex justify-start laptop:mt-20 laptop:flex-row w-full  mob:flex-col my-7 align-middle ">
-      <h1 className="text-3xl laptop:w-1/3 mob:w-full">Tools & Platform</h1>
-      <div className="laptop:w-2/3 mob:w-full  flex flex-wrap">
-        {toolNPlatform?.map((ls,index)=><div
-                            key={index}
-          className={`w-fit p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
-            mounted && theme === "dark"
-              ? "hover:bg-slate-800"
-              : "hover:bg-slate-50"
-          } hover:scale-105 link`}
-        >
-          <h1 className="text-xl">{ls}</h1>
-        </div>)}
+      <div className="flex justify-start laptop:mt-20 laptop:flex-row w-full  mob:flex-col my-7 align-middle ">
+        <h1 className="text-3xl laptop:w-1/3 mob:w-full">Tools & Platform</h1>
+        <div className="laptop:w-2/3 mob:w-full  flex flex-wrap">
+          {toolNPlatform?.map((ls, index) => <div
+            key={index}
+            className={`w-fit p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${mounted && theme === "dark"
+                ? "hover:bg-slate-800"
+                : "hover:bg-slate-50"
+              } hover:scale-105 link`}
+          >
+            <h1 className="text-xl">{ls}</h1>
+          </div>)}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
