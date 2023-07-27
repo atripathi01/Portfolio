d88bde672bdcf7468caa58a91d4c27af9aa6889a
Ayush Tripathi <ayushtripathi5014@gmail.com>
2023-07-27 21:41:49 +0530

    Update SkillsCard.js

--- components/Skills/SkillsCard.js
+++ components/Skills/SkillsCard.js
@@ -13,7 +13,8 @@ const SkillsCard = ({ toolNPlatform, webdeveloper }) => {
     <div className="flex justify-start laptop:flex-row mob:flex-col  my-7 align-middle ">
       <h1 className="text-3xl laptop:w-1/3 mob:w-full">Web-Development</h1>
       <div className=" laptop:w-2/3 mob:w-full flex flex-wrap">
-        {webdeveloper?.map((list)=><div
+        {webdeveloper?.map((list,index)=><div
+                           key={index}
           className={`w-fit p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
             mounted && theme === "dark"
               ? "hover:bg-slate-800"
@@ -28,7 +29,8 @@ const SkillsCard = ({ toolNPlatform, webdeveloper }) => {
     <div className="flex justify-start laptop:mt-20 laptop:flex-row w-full  mob:flex-col my-7 align-middle ">
       <h1 className="text-3xl laptop:w-1/3 mob:w-full">Tools & Platform</h1>
       <div className="laptop:w-2/3 mob:w-full  flex flex-wrap">
-        {toolNPlatform?.map((ls)=><div
+        {toolNPlatform?.map((ls,index)=><div
+                            key={index}
           className={`w-fit p-2 mob:p-4 rounded-lg transition-all ease-out duration-300 ${
             mounted && theme === "dark"
               ? "hover:bg-slate-800"
